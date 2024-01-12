07252103e0392290375c8483bfa567bb08e0560c
axel-dev <aherreram22@miumg.edu.gt>
2024-01-11 21:39:11 -0600

    nose

--- tasks/models.py
+++ tasks/models.py
@@ -105,7 +105,7 @@ def delete_Photos(sender, instance, **kwargs):
         
         default_storage.delete(file_name)
     except Exception as e:
-        print(f"Error al eliminar el archivo para municipalidad {instance.munici_id}: {e}")
+        print(f"Error al eliminar el archivo para municipalidad {instance.id}: {e}")
     
 
 class Videos(models.Model):
@@ -124,5 +124,5 @@ def delete_Videos(sender, instance, **kwargs):
         
         default_storage.delete(file_name)
     except Exception as e:
-        print(f"Error al eliminar el archivo para municipalidad {instance.munici_id}: {e}")
+        print(f"Error al eliminar el archivo para municipalidad {instance.id}: {e}")
        
